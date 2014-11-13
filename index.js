c63ff8142e73028f20c050cc37f5a84487589865
smituk <sergey.stamat@gmail.com>
2014-11-13 12:17:02 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var liqpay = function(op) {
+  this.op = op;
+  return this;
+};
+
+liqpay.prototype.charge = function() {
+  //TODO:
+};
+
+var api = function(method, data, callback) {
+  //TODO:
+};
+
+module.exports = liqpay;
